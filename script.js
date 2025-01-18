11b69ace2c2cbd8542ff3805c3dd9bc5e30dbb0b
Tindap123 <filippysklo@icloud.com>
2025-01-18 19:05:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-document.addEventListener(„DOMContentLoaded”, function() {
-  const loginForm = document.getElementById(„loginForm”);
 
-  loginForm.addEventListener(„submit”, function(event) {
-    event.preventDefault();  
-
-    const email = document.getElementById(„email”).value;
-    const password = document.getElementById(„password”).value;
-
-   
-    if (email && password) {
-      alert(„Logowanie udane!”);
-    } else {
-      alert(„Proszę wypełnić wszystkie pola!”);
-    }
-  });
-});
