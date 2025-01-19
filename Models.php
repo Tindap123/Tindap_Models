b62ae8c2f134632cccae3f85c3a30149747ccb0c
Tindap123 <filippysklo@icloud.com>
2025-01-19 15:06:14 +0100

    Update Models.php

--- Models.php
+++ Models.php
@@ -4,9 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "tindup_models";
 
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 
 if ($conn->connect_error) {
     die("Połączenie nieudane: " . $conn->connect_error);
@@ -27,3 +25,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $conn->close();
 ?>
+
