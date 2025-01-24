45de4ca114aae0ec1e6cd92a30194669e0aa894b
Tindap123 <filippysklo@icloud.com>
2025-01-24 14:53:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,9 @@
 const modelButton = document.getElementById(‚modelButton’);
 const firmButton = document.getElementById(‚firmButton’);
 
-
 modelButton.addEventListener(‚click’, () => {
     window.location.href = ‚model-login.html’;
 });
-
 
 firmButton.addEventListener(‚click’, () => {
     window.location.href = ‚firm-login.html’;
